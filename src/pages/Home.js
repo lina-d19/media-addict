f998eec5f0a5e4326f43b8ff37c64c9caa5bc269
lina-d777 <ldbagh@uwo.ca>
2022-08-03 19:34:37 -0400

    removed redundancies

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import Header from '../components/Header'
-import Reddit from './Reddit'
-import Spotify from './Spotify'
 import Twitter from './Twitter'
 
 const Home = () => {
