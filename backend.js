6bf4c48a5d1c7cb9d987d5367413a647f9e94fd1
lina-d777 <ldbagh@uwo.ca>
2022-08-05 14:18:48 -0400

    modified backend

--- backend.js
+++ backend.js
@@ -123,11 +123,4 @@ app.get('/newsarticles', async (req, res) => {
 
 })
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-    });
-  }
-  
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
